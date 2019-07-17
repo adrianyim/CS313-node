d489b2cbc61d9bb15c8788505484518acdc2734b
adrianyim <adriany93@gmail.com>
2019-07-16 23:30:13 -0600

    Updated select and insert functions

--- db/myDB.sql
+++ db/myDB.sql
@@ -1,77 +1,89 @@
 -- Create section
-
 CREATE TABLE users (
-    user_id SERIAL NOT NULL,
-    user_name VARCHAR(225) UNIQUE NOT NULL CONSTRAINT users_pk PRIMARY KEY,
+    user_id serial NOT NULL,
+    user_name VARCHAR(225) UNIQUE NOT NULL CONSTRAINT users_name_pk PRIMARY KEY,
     password VARCHAR(255) NOT NULL, 
     gender VARCHAR(1)
 );
 
 CREATE TABLE items (
-    item_id SERIAL NOT NULL CONSTRAINT items_pk PRIMARY KEY,
+    item_id serial NOT NULL CONSTRAINT items_id_pk PRIMARY KEY,
     item VARCHAR(45) NOT NULL, 
     item_type VARCHAR(45) NOT NULL,
     remark VARCHAR(45),
-    cost_id INT NOT NULL CONSTRAINT item_fk REFERENCES cost(cost_id),
-    date_id INT NOT NULL CONSTRAINT item_fk2 REFERENCES date(date_id),
+    cost_id serial NOT NULL CONSTRAINT items_fk_1 REFERENCES cost(cost_id),
+    date_id serial NOT NULL CONSTRAINT items_fk_2 REFERENCES date(date_id),
     user_name VARCHAR(225) NOT NULL CONSTRAINT items_fk3 REFERENCES users(user_name)
 );
 
 CREATE TABLE cost (
-    cost_id SERIAL NOT NULL CONSTRAINT cost_pk PRIMARY KEY,
+    cost_id serial NOT NULL CONSTRAINT cost__id_pk PRIMARY KEY,
     cost DECIMAL NOT NULL,
     cost_type VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE date (
-    date_id SERIAL NOT NULL UNIQUE CONSTRAINT date_pk PRIMARY KEY,
+    date_id serial NOT NULL UNIQUE CONSTRAINT date_id_pk PRIMARY KEY,
     date date NOT NULL
 );
 
+
 CREATE TABLE total (
-    id SERIAL NOT NULL CONSTRAINT total_pk PRIMARY KEY,
+    id serial NOT NULL CONSTRAINT total_pk PRIMARY KEY,
     total DECIMAL NOT NULL
 );
 
--- Insert/Select section
-INSERT INTO users (user_name, password, gender)
-    VALUES  ('admin', 'admin', 'M'),
-            ('tester', 'tester', 'M');
+-- Insert section
+INSERT INTO users (user_id, user_name, password, gender)
+    VALUES  (DEFAULT, 'admin', 'admin', 'M'),
+            (DEFAULT, 'tester', 'tester', 'M');
 
-INSERT INTO date (date)
-    VALUES  ('05-29-2019'),
-            ('06-01-2019'),
-            ('06-01-2019'),
-            ('06-03-2019');
+INSERT INTO date (date_id, date)
+    VALUES  (DEFAULT, '07-10-2019'),
+            (DEFAULT, '07-05-2019'),
+            (DEFAULT, '07-11-2019'),
+            (DEFAULT, '07-11-2019');
 
-INSERT INTO cost (cost, cost_type)
-    VALUES  (20.99, 'Expense'),
-            (11.02, 'Expense'),
-            (504.32, 'Income'),
-            (30.51, 'Expense');
+INSERT INTO cost (cost_id, cost, cost_type)
+    VALUES  (DEFAULT, 20.99, 'Expense'),
+            (DEFAULT, 11.02, 'Expense'),
+            (DEFAULT, 504.32, 'Income'),
+            (DEFAULT, 30.51, 'Expense');
 
-INSERT INTO items (item, item_type, remark, cost_id, date_id, user_name) 
-    VALUES  ('Walmark', 'Food', 'shoes', 1, 1, 'admin'),
-            ('BBQ', 'Food', '', 2, 2, 'tester'),
-            ('Admin salary', 'Salaries and wages', 'May', 3, 3, 'admin'),
-            ('Walmart', 'Food', 'ice cream', 4, 4, 'admin');
+INSERT INTO items (item_id, item, item_type, remark, cost_id, date_id, user_name) 
+    VALUES  (DEFAULT, 'Walmark', 'Food', 'shoes', DEFAULT, DEFAULT, 'admin'),
+            (DEFAULT, 'BBQ', 'Food', '', DEFAULT, DEFAULT, 'tester'),
+            (DEFAULT, 'Admin salary', 'Salaries and wages', 'May', DEFAULT, DEFAULT, 'admin'),
+            (DEFAULT, 'Walmart', 'Food', 'ice cream', DEFAULT, DEFAULT, 'admin');
 
-INSERT INTO items (item_id, item, item_type, cost, cost_type, remark, date, user_name) 
-    VALUES (DEFAULT, 'Rent', 'Utility Expenses', 573, 'Income', '??', current_timestamp, 'adrian');
+-- ------
+INSERT INTO items (item_id, item, item_type, remark, cost_id, date_id, user_name) 
+    VALUES (DEFAULT, 'Rent', 'Utility Expenses', '??', (SELECT cost_id from cost WHERE cost_id = 5), (SELECT date from date d WHERE c.date_id = 5), 'tester');
+
+INSERT INTO items (item_id, item, item_type, remark, cost_id, date_id, user_name) 
+    VALUES (DEFAULT, 'Rent', 'Utility Expenses', '??', DEFAULT, DEFAULT, 'tester');
+
+INSERT INTO cost (cost_id, cost, cost_type)
+    VALUES  (DEFAULT, 570, 'Expense');
+
+INSERT INTO date (date_id, date)
+    VALUES  (DEFAULT, current_timestamp);
 
 
 INSERT INTO totals (id, total, user_id, item_id)
 VALUES (DEFAULT, 10, (SELECT user_id FROM users WHERE user_name='tester'), (SELECT item_id FROM items WHERE item_type='food'));
+-- ------
 
--- ------- --
--- select items, cost, date
+-- select section
 SELECT i.user_name, i.item_id, i.item, i.item_type, i.remark, (SELECT c.cost FROM cost c WHERE c.cost_id = i.cost_id) AS cost, (SELECT c.cost_type FROM cost c WHERE c.cost_id = i.cost_id) AS cost_type, (SELECT d.date FROM date d WHERE d.date_id = i.date_id) AS date FROM items i WHERE i.user_name = 'admin' ORDER BY date;
 
 SELECT i.user_name, I.item_id, i.item, i.item_type, i.remark, c.cost, c.cost_type, d.date FROM items i, INNER JOIN cost c ON i.cost_id = c.cost_id INNER JOIN date d ON i.date_id = d.date_id WHERE i.user_name = 'admin';
-
--- ------- --
-SELECT u.user_name, i.item_id, i.item, i.item_type, c.cost, c.cost_type, i.remark, d.date FROM users u, items i, cost c, date d WHERE u.user_name='admin' ORDER BY d.date;
     
+-- delete section
+DELETE FROM users WHERE user_id IN (34-46);
+
+DELETE FROM users WHERE user_name="adrian";
+
 -- UNION JOIN
 SELECT user_name FROM users WHERE user_name='admin'
 UNION ALL
@@ -82,7 +94,7 @@ UNION ALL
 SELECT date FROM date WHERE user_name='admin';
 
 
--- Command my DB
+-- Command DB
 CREATE USER adrianyim WITH PASSWORD 'adrianyim';
 GRANT SELECT, INSERT, UPDATE, DELETE ON users TO adrianyim;
 GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO adrianyim;
@@ -99,12 +111,8 @@ UPDATE items
 SET item = 'Update item1', item_type='Changed item type', cost=9090, cost_type='Income', remark='Testing the updates'
 WHERE item_id = 16;
 
-ALTER TABLE users
-    ALTER COLUMN password NOT NULL;
-
-DELETE FROM users WHERE user_id IN (34-46);
-
-DELETE FROM users WHERE user_name="adrian";
+ALTER TABLE items
+    ALTER COLUMN cost_id SERIAL;
 
 -- Set timezone/date style to mountain standard time
 SET TIMEZONE='MST';
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 // const session = require("express-session");
-// const bodyparse = require("body-parse");
+const bodyparser = require("body-parser");
 const pg = require("pg");
-const connectionString = process.env.DATABASE_URL 
-// || "postgres://adrianyim:adrianyim@localhost:5432/budgetkeeper_db";
+const connectionString = "postgres://adrianyim:adrianyim@localhost:5432/budgetkeeper_db";
+//process.env.DATABASE_URL || 
 
 //Use server, public file
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyparser.urlencoded({ extended: false }));
 
 //Set server
 app.set("views", path.join(__dirname, "views"));
@@ -21,25 +22,31 @@ app.get("/", (req, res) => {
     res.render("pages/BudgetKeeper-home");
 });
 
-app.get("/getItems", getItems);
-// app.get("/getItems/:id", getItems);
+// Select
+app.get("/selectItems", selectItems);
+// app.get("/selectItems/:id", selectItems);
+
+// Insert
+app.post("/insertItems", insertItems);
+
+// Update
+// app.put("/updateItems", updateItems);
+
+// Delete
+// app.delete("/deleteItems", deleteItems);
 
 //Listen server
 app.listen(app.get("port"), () => {
     console.log("Listening for connections on: ", app.get("port"));
 });
 
+// Calling server functions
+// Select funtion
+function selectItems(req, res) {
+    console.log("In the selectItems function");
 
-// Functions
-function getItems(req, res) {
-    console.log("In the getItems function");
-
-    let username = req.query.username;
-    // let id = req.params.id;
-    console.log("The username is : ", username);
-
-    getItemsDB(username, (error, result) => {
-        console.log("The result from getItemsDB from the DB is: " , result);
+    selectDB((error, result) => {
+        console.log("The result from selectItems from the DB is: " , result);
 
         if (error || result == null || result.length == 0) {
             console.log("Why got error?", error, result, result.length);
@@ -50,24 +57,23 @@ function getItems(req, res) {
     });
 }
 
-function getItemsDB(username, callback) {
-    console.log("In getItemsDB function with username: ", username);
-
-    // let sql = "SELECT i.user_name, i.item_id, i.item, i.item_type, i.remark, (SELECT c.cost FROM cost c WHERE c.cost_id = i.cost_id) AS cost, (SELECT c.cost_type FROM cost c WHERE c.cost_id = i.cost_id) AS cost_type, (SELECT d.date FROM date d WHERE d.date_id = i.date_id) AS date FROM items i WHERE i.user_name = 'admin' ORDER BY date;";
+// SELECT query funtion
+function selectDB(callback) {
+    console.log("In selectDB function");
 
     let client = new pg.Client(connectionString);
 
-    client.connect((err => {
+    client.connect((err) => {
         if (err) {
             console.log("Error with DB!");
             console.log(err);
             callback(err, null);
         }
 
-        let sql = "SELECT * FROM items WHERE user_name=$1";
-        let params = [username];
+        let sql = "SELECT i.user_name, i.item_id, i.item, i.item_type, i.remark, (SELECT c.cost FROM cost c WHERE c.cost_id = i.cost_id) AS cost, (SELECT c.cost_type FROM cost c WHERE c.cost_id = i.cost_id) AS cost_type, (SELECT d.date FROM date d WHERE d.date_id = i.date_id) AS date FROM items i ORDER BY date";
+        // let params = [username];
 
-        let query = client.query(sql, params, (err, result) => {
+        client.query(sql, (err, result) => {
             client.end((err) => {
                 if (err) throw err;
             });
@@ -80,5 +86,121 @@ function getItemsDB(username, callback) {
             console.log("Result is: " + JSON.stringify(result.rows));
             callback(null, result.rows);
         });
-    }))
+    });
+}
+
+//Insert funtion
+function insertItems(req, res) {
+    console.log("In insertItems function");
+
+    // Retrieve data from client side
+    let item = req.body.item;
+    let item_type = req.body.item_type;
+    let cost = req.body.cost;
+    let cost_type = req.body.cost_type;
+    let remark = req.body.remark;
+
+    console.log("The retrieve data are: ");
+    console.log("Item: ", item);
+    console.log("Item_type: ", item_type);
+    console.log("Cost: ", cost);
+    console.log("Cost_type: ", cost_type);
+    console.log("Remark: ", remark);
+
+    insertDB(item, item_type, cost, cost_type, remark, (error, result) => {
+        console.log("The result from insertItems from the DB is: " , result);
+
+        if (error || result == null) {
+            console.log("Why got error?", error, result);
+            res.status(500).json({success: false, data: error});
+        } else {
+            res.send(result);
+        }
+        
+    });
+}
+
+// INSERT query function
+function insertDB(item, item_type, cost, cost_type, remark, callback) {
+    console.log("In insertDB function");
+
+    // Create client object
+    let client = new pg.Client(connectionString);
+
+    // Connect to DB
+    client.connect((err) => {
+        if (err) {
+            console.log("Error with connecting to DB: ", err);
+            callback(err, null);
+        }
+
+        console.log("DB corrected!");
+
+        let combinedResult;
+
+        // SQL queries
+        let sql_items = "INSERT INTO items (item_id, item, item_type, remark, cost_id, date_id, user_name) VALUES (DEFAULT, $1, $2, $3, DEFAULT, DEFAULT, 'tester')";
+
+        let sql_cost = "INSERT INTO cost (cost_id, cost, cost_type) VALUES (DEFAULT, $1, $2)";
+        
+        let sql_date = "INSERT INTO date (date_id, date) VALUES (DEFAULT, current_timestamp)";
+
+        let cost_params = [cost, cost_type];
+
+        let items_params = [item, item_type, remark];
+
+        // Inserting into cost table
+        client.query(sql_cost, cost_params, (err, result) => {
+            console.log("Inerting into COST");
+            console.log(cost_params);
+
+            if (err) {
+                console.log("ERROR: Problem with insert query into cost table: ", err);
+                callback(err, null);
+            }
+
+            // Query result
+            console.log(result);
+            combinedResult += result; 
+
+            // Inserting into date table
+            client.query(sql_date, (err, result) => {
+                console.log("Inerting into DATE");
+                
+                if (err) {
+                    console.log("ERROR: Problem with insert query into date table: ", err);
+                    callback(err, null);
+                }
+
+                // Query result
+                console.log(result);
+                combinedResult += result; 
+
+                // Inserting into items table
+                client.query(sql_items, items_params, (err, result) => {
+                    console.log("Inerting into ITEMS");
+                    console.log(items_params);
+
+                    if (err) {
+                        console.log("ERROR: Problem with insert query into items table: ", err);
+                        callback(err, null);
+                    }
+
+                    // Query result
+                    console.log(result);
+                    combinedResult += result; 
+
+                    callback(null, combinedResult);
+
+                    // End the query
+                    client.end((err) => {
+                        if (err) {
+                            console.log("Error with ending the query: ", err);
+                            throw err;
+                        }
+                    });
+                });
+            });
+        });
+    });
 }
